055a086edfda001166f2114f24fd86abd8e027c6
Jimena Miguez <jimeblue69@gmail.com>
2021-09-07 19:55:45 +0200

    Create app skeleton (#1)
    * Install fonts via link in index.html
    
    * Reset default browser styles with normalized.css
    
    * Add global styles
    
    * Add router with routes / and /search
    
    * Create folder Fonts inside Public and add fonts
    
    * Change meta name and content and remove favicon in index.html
    
    * Install styled-components and wrap children in App.js with ThemeProvider
    
    * Replace normalize.css with style-normalized package
    
    * Add GlobalStyle.js and theme.js in src
    
    * Add <Route> with 404 - Not Found in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import { Normalize } from 'styled-normalize';
+import GlobalStyles from './GlobalStyles';
+import theme from './theme';
+import './index.css';
+import Home from './components/Home';
+import Search from './components/Search';
 
 function App() {
   return (
-    <div>
-      App Placeholder
-    </div>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Normalize />
+        <GlobalStyles />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/search">
+            <Search />
+          </Route>
+          <Route>404 - Not Found</Route>
+        </Switch>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/Search.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Search = () => (
+  <div>
+    <h1>This is the Search page</h1>
+  </div>
+);
+
+export default Search;
