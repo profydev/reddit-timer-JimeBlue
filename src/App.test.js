31ba23304d3f0efbd4e6238acdf22e6505e08353
Jimena <jimeblue69@gmail.com>
2021-09-18 17:37:16 +0200

    Add test for header logo link

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,22 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
-test('renders App', () => {
-  render(<App />);
+describe('Header', () => {
+  test('logo link points to the correct page', () => {
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const link = screen.getByRole('link', { name: /Application logo/i });
+    screen.debug(link);
+    userEvent.click(link);
+
+    expect(
+      screen.getByRole('heading', { name: /This is the HomePage/i }),
+    ).toBeInTheDocument();
+  });
 });
