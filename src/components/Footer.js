dc16c02323e2719af0a92e0287b989c539fac764
Jimena <jimeblue69@gmail.com>
2022-02-06 15:48:01 +0100

    Style footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,45 @@ import { Link } from 'react-router-dom';
 import sign from '../assets/sign.svg';
 
 const StyledFooter = styled.footer`
-  color: blue;
+  width: 100%;
+
+  ul {
+    list-style-type: none;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+  }
+
+  li:not(:last-child) {
+    margin-bottom: 1rem;
+  }
+
+  li:nth-child(1) {
+    padding-left: 1rem;
+  }
+
+  li:nth-child(2) {
+    padding-left: 1.625rem;
+  }
+
+  li:nth-child(3) {
+    padding-left: 0.9rem;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  @media screen and (min-width: 480px) {
+    padding: 0.5rem 0 0.5rem;
+
+    ul {
+      flex-direction: row;
+      align-items: flex-start;
+    }
+  }
 `;
 
 const Footer = () => (
