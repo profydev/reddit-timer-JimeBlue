0a3993c5bb0dff710aa81d244ade614f1d461ad1
Jimena <jimeblue69@gmail.com>
2022-02-06 13:49:17 +0100

    Add content and links to Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,27 @@
 import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import sign from '../assets/sign.svg';
+
+const StyledFooter = styled.footer`
+  color: blue;
+`;
 
 const Footer = () => (
-  <div>
-    <h1>This is the Footer</h1>
-  </div>
+  <StyledFooter>
+    <ul>
+      <li>
+        <a href="https://profy.dev/employers">profy.dev</a>
+      </li>
+      <li>
+        <Link to="/">
+          <img src={sign} alt="footer logo" />
+        </Link>
+      </li>
+      <li>
+        <Link to="/terms">Terms & Privacy</Link>
+      </li>
+    </ul>
+  </StyledFooter>
 );
 export default Footer;
